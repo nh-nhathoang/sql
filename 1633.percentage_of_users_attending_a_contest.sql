9c39a9871480ef456b13684758b5a5077e3db55e
Hoang Le <lnnhoang01@gmail.com>
2026-07-13 16:36:52 +0300

    add solutions using aggregation functions

---
+++ 1633.percentage_of_users_attending_a_contest.sql
@@ -0,0 +1,21 @@
+-- Table: Users
+-- user_id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the name and the id of a user.
+ 
+
+-- Table: Register
+
+-- (contest_id, user_id) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table contains the id of a user and the contest they registered into.
+ 
+
+-- Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+-- Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+-- Write your MySQL query statement below
+SELECT contest_id, 
+       ROUND(COUNT(user_id) * 100 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register 
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id;
